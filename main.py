da0ef94486b3f42df5ceef3f16b5992436186886
Raja Vamsi <24pa5a5703@vishnu.edu.in>
2026-03-01 21:37:52 +0530

    Fresh secure start

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify, render_template
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # load .env file
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    prompt = request.json.get("message")
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    return jsonify({"reply": response.text})
+
+if __name__ == "__main__":
+    app.run(port=5000)
